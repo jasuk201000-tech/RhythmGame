0e7a603f7d8aba4977d2cf37a8d042e8f5fea492
jasu <jasuk201000@gmail.com>
2026-08-20 08:01:06 +1000

    beat tempo

---
+++ Assets/Scripts/BeatScroller.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class BeatScroller : MonoBehaviour
+{
+    public float BeatTempo;
+
+    public bool HasStarted;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        BeatTempo = BeatTempo / 60f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!HasStarted)
+        {
+            if (Input.anyKeyDown)
+            {
+                HasStarted = true;
+            }
+        }
+        else
+        {
+            transform.position -= new Vector3(0f, BeatTempo * Time.deltaTime, 0f);
+        }
+    }
+}
