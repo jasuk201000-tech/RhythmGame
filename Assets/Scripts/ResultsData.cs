2705b4fde35cf801fc2fe3fe62d0e467a9cdbf21
jasu <jasuk201000@gmail.com>
2026-08-29 10:31:25 +1000

    adding results data

--- Assets/Scripts/ResultsData.cs
+++ Assets/Scripts/ResultsData.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 
-public class ResultsData
+public static class ResultsData
 {
-    
+    public static int perfects;
+    public static int greats;
+    public static int goods;
+    public static int misses;
+    public static float accuracy;
+    public static int longestCombo;
 }
